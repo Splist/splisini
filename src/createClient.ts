ee3df60ce4abed09a112be0807be216bfc2eddfd
Jdender~ <jdenderplays@gmail.com>
2019-10-27 09:56:55 -0400

    Fix: fix createClient to not print only [object Object] on error

--- src/createClient.ts
+++ src/createClient.ts
@@ -31,10 +31,10 @@ export const createClient = (options: CreateClientOptions) => {
     const errorLink = onError(({ graphQLErrors, networkError }) => {
         if (graphQLErrors)
             graphQLErrors.map(({ message, locations, path }) =>
-                console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+                console.error('[GraphQL error]: Message:', message, 'Location:', locations, 'Path:', path)
             );
 
-        if (networkError) console.error(`[Network error]: ${networkError}`);
+        if (networkError) console.error('[Network error]:', networkError);
     });
 
     const httpLink = new HttpLink({
